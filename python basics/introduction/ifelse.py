d5e7065df72a855495124ba2007b8409b73555c2
PROSOLVER <94039049+PranavShrivastava25@users.noreply.github.com>
2022-03-02 00:59:07 +0530

    Update ifelse.py

--- python basics/introduction/ifelse.py
+++ python basics/introduction/ifelse.py
@@ -1,1 +1,26 @@
+#!/bin/python
+'''
+given a number n :
+if:
+If n is odd, print Weird
+If n is even and in the inclusive range of  to , print Not Weird
+If n is even and in the inclusive range of  to , print Weird
+If n is even and greater than , print Not Weird
+'''
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+n=input();
+if n%2 is not 0:
+    print "Weird"
+elif n%2 is  0 and n<=20 and n>=6:
+    print "Weird"
+else :
+    print "Not Weird"
 
